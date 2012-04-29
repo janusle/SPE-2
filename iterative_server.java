7f5cb1e077db3a0f0c24a57bc58f22ae794f149d
janusle <janusle@gmail.com>
2012-04-29 22:08:26 +1000

    initial commit

---
+++ iterative_server.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.net.*;
+import javax.net.ssl.*;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+
+public class iterative_server {
+
+    private int port;
+
+
+    public static void main(String[] args) {
+          
+         iterative_server is = new iterative_server(9999);
+         is.run();
+     
+    }
+
+    private String getCurrentTime(){
+
+        Calendar currentDate = Calendar.getInstance();
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss dd:mm:yyyy");
+        return formatter.format( currentDate.getTime() );
+
+    }
+  
+    public iterative_server( int port ){
+      
+        this.port = port;
+
+    }
+
+    
+    public void run(){
+
+        try{
+          
+           SSLServerSocketFactory ssf = (SSLServerSocketFactory)SSLServerSocketFactory.getDefault();
+           SSLServerSocket ss = (SSLServerSocket)ssf.createServerSocket( this.port );
+           while( true ){
+              
+               SSLSocket s = (SSLSocket) ss.accept();
+               BufferedReader in = new BufferedReader(new InputStreamReader( s.getInputStream() ) );
+               PrintWriter out = new PrintWriter( s.getOutputStream(), true);
+               String line = null;
+               while ( ( line = in.readLine() ) != null ){
+                  
+                   if ( line.equals( "What is the time?" ) ){
+                      System.out.println("Request from " + s.getInetAddress().getHostAddress() );
+                      String currentTime = this.getCurrentTime();                    
+                      out.println( currentTime ); 
+                   }
+                   else{
+                     System.out.println("Invalid request from " + s.getInetAddress().getHostAddress() );
+                   }
+
+               }
+               out.close();
+               in.close();
+               s.close();
+
+           }
+
+
+        }catch(Exception e){
+
+            System.out.println( e.getMessage() );
+            System.exit(-1);
+        }
+
+
+    }
+
+
+}
